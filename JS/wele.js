5abb9503c1262f2ab11d84d41e4f7df65dcd6d07
Aecaay <45263997+Aecaay@users.noreply.github.com>
2020-12-24 15:49:23 +0800

    Add files via upload

--- JS/wele.js
+++ JS/wele.js
@@ -1,5 +1,5 @@
 const msg = "还没写";
-function handleMessage(message = msg) {
+function hMes(message = msg) {
   const parentDiv = document.createElement("div");
   const div = document.createElement("div");
   div.className = "mes-ct";
@@ -10,5 +10,30 @@ function handleMessage(message = msg) {
 
 setTimeout(() => {
   parentDiv.remove();
-  }, 2000);
+  }, 1000);
   }
+
+window.onload=function(){
+  var text=document.getElementById("ftext");
+  text.onmouseover = function(){
+    move(10)
+  };
+  text.onmouseout = function(){
+    move(-10)
+  };
+}
+
+var time;
+
+function move(spd){
+  clearInterval(time);
+  var text = document.getElementById('ftext');
+  time = setInterval(function(){
+    console.log(text.offsetLeft);
+    if(text.offsetLeft == -100+10*spd){
+      clearInterval(time);
+    }else{
+      text.style.left=text.offsetLeft+spd+'px';
+    }
+  },10);
+}
